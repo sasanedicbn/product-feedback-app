b5d2a80b27ec5ef01d3a7ce04773eaa6e04044f2
Sasa Nedic <svetasmak0@gmail.com>
2024-11-03 12:00:48 +0100

    Set configuration for redux-toolkit

---
+++ src/components/store/slices/categorySlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categorySlice = createSlice({
+  name: 'category',
+  initialState: {
+    category: '',
+  },
+  reducers: {
+    
+}});
+
+export const {  } = categorySlice.actions;
+export default categorySlice.reducer;
