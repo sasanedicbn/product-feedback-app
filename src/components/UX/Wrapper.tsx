95069a570c5895940598cd79f4edda38f29103cc
Sasa Nedic <svetasmak0@gmail.com>
2024-11-03 08:52:14 +0100

    Create Wrapper reusable component

---
+++ src/components/UX/Wrapper.tsx
@@ -0,0 +1,14 @@
+
+const Wrapper = ({ children, type }) => {
+    const base = 'flex justify-between my-8 mx-auto';
+    const styles = {
+        pageWrapper: `${base} w-full sm:w-1/2 md:w-1/3 lg:w-1/4`, 
+    };
+    return (
+      <div className={styles[type]}>
+        {children}
+      </div>
+    );
+};
+
+export default Wrapper;
