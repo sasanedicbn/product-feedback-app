5776a7024434f15d744e88b1cb4fdff3d739a730
Sasa Nedic <svetasmak0@gmail.com>
2024-11-04 16:38:09 +0100

    Wrap up TitleSideBar component in wrapper

--- src/components/UX/Wrapper.tsx
+++ src/components/UX/Wrapper.tsx
@@ -2,8 +2,11 @@ import { WrapperProps } from "../types/types";
 
 const Wrapper = ({ children, type }:WrapperProps) => {
     const base = 'flex justify-between my-8 mx-auto';
+    const baseSideBar = 'h-[150px] rounded-xl p-6 bg-gradient-to-br'
     const styles = {
         pageWrapper: `${base} w-full sm:w-1/2 md:w-1/3 lg:w-1/4`, 
+        sideBarTitle: `${baseSideBar} from-blue-400 via-teal-300 to-green-300`,
+        sideBarItem: `${baseSideBar}`
     };
     return (
       <div className={styles[type]}>
