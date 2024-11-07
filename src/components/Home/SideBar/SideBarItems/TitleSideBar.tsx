c38407e2441296cd105f911dea343c51ba9d60ee
Sasa Nedic <svetasmak0@gmail.com>
2024-11-07 14:07:45 +0100

    Add animation in TitleSideBar component ad SingleCategory component

--- src/components/Home/SideBar/SideBarItems/TitleSideBar.tsx
+++ src/components/Home/SideBar/SideBarItems/TitleSideBar.tsx
@@ -1,11 +1,19 @@
+import { motion } from "framer-motion";
 import Wrapper from "../../../UX/Wrapper";
 
 const TitleSideBar = () => {
     return (
         <Wrapper type="sideBarTitle">
-            <p className="text-xl text-gray-500 font-medium ">Feedback Board</p>
+            <motion.p
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+                className="text-xl text-gray-500 font-medium"
+            >
+                Feedback Board
+            </motion.p>
         </Wrapper>
-    )
+    );
 }
 
 export default TitleSideBar;
--- src/components/UX/SingleCategory.tsx
+++ src/components/UX/SingleCategory.tsx
@@ -1,9 +1,15 @@
-const SingleCategory = ({ category}) => {
+import { motion } from "framer-motion";
+
+const SingleCategory = ({ category }) => {
     return (
-        <li 
-            className="bg-[#f2f4ff]  text-xs rounded-lg px-3 py-1.5 font-semibold leading-[19px] list-none text-[#4661e6] mt-3 mr-3 cursor-pointer hover:bg-blue-200">
+        <motion.li
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            className="bg-[#f2f4ff] text-xs rounded-lg px-3 py-1.5 font-semibold leading-[19px] list-none text-[#4661e6] mt-3 mr-3 cursor-pointer hover:bg-blue-200"
+        >
             {category}
-        </li>
+        </motion.li>
     );
 }
 
