c6de7faed6020c3ddcb6b40d26b880abf65cf1b4
Sasa Nedic <svetasmak0@gmail.com>
2024-11-09 17:54:40 +0100

    Fetch data from supabase in Details component

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { fetchFeedbackById } from "../../supabase/supabaseFunctions";
 
 const Details = () => {
     const {id} = useParams()
+
+    const fetchFeedBack = async () => {
+      const feedBack = await fetchFeedbackById(id)
+      console.log(feedBack, 'feedback')
+    }  
+    useEffect(() => {
+    fetchFeedBack()
+    }, [id])
     console.log(id, 'id iz paramsa')
+    // ContentItem
     return(
         <div>
             Details
