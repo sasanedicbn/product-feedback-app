83c53cdc7ffa3843edccfbe846725544ae0badd5
Sasa Nedic <svetasmak0@gmail.com>
2024-11-10 15:31:01 +0100

    Create come changes in FeedBackComments component

--- src/components/Details/feedBackComments/FeedBackComments.tsx
+++ src/components/Details/feedBackComments/FeedBackComments.tsx
@@ -7,19 +7,19 @@ const FeedBackComments = ({ feedback }) => {
     if (!Comments) return <p>No comments available.</p>;
 
     return (
-        <Wrapper type="feedBackContainer">
-            <div className="flex bg-red-200 w-full ">
+        <Wrapper type="feedBackComments">
+            <div className="flex w-full border-b border-gray-200 pb-8 mb-8">
                 <img
                     src={Comments.user_image}
                     alt="User"
-                    className="w-12 h-12 rounded-full mr-4"
+                    className="w-12 h-12 rounded-full mr-4 object-cover"
                 />
                 
                 <div className="flex-1">
                     <div className="flex justify-between items-center">
                         <div>
-                            <p className="font-semibold text-dark-blue">{Comments.user}</p>
-                            <p className="text-gray-500 text-sm">{Comments.user_name}</p>
+                            <p className="font-semibold text-blue-950">{Comments.user}</p>
+                            <p className="text-gray-500 text-sm font-light">{Comments.user_name}</p>
                         </div>
 
                         <Button type="replay" >
@@ -27,7 +27,29 @@ const FeedBackComments = ({ feedback }) => {
                         </Button>
                     </div>
                     
-                    <p className="mt-2 text-gray-700">{Comments.user_comment}</p>
+                    <p className="text-gray-500 text-sm font-light mt-4">{Comments.user_comment}</p>
+                </div>
+            </div>
+            <div className="flex w-full">
+                <img
+                    src={Comments.user_image}
+                    alt="User"
+                    className="w-12 h-12 rounded-full mr-4 object-cover"
+                />
+                
+                <div className="flex-1">
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <p className="font-semibold text-blue-950">{Comments.user}</p>
+                            <p className="text-gray-500 text-sm font-light">{Comments.user_name}</p>
+                        </div>
+
+                        <Button type="replay" >
+                            Reply
+                        </Button>
+                    </div>
+                    
+                    <p className="text-gray-500 text-sm font-light mt-4">{Comments.user_comment}</p>
                 </div>
             </div>
         </Wrapper>
--- src/components/types/types.ts
+++ src/components/types/types.ts
@@ -37,7 +37,7 @@ export type OptionProps = {
 
 export type WrapperProps = {
     children: ReactNode; 
-    type: 'pageWrapper' | 'sideBarTitle' |  'sideBarItem' | 'content' | 'feedBackContainer'; 
+    type: 'pageWrapper' | 'sideBarTitle' |  'sideBarItem' | 'content' | 'feedBackContainer' | 'feedBackComments'; 
   }
   
 export type RoadmapItemProps = {
