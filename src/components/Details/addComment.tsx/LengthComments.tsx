52fb3b6b454844516f2be3f8af81326f9ab33c3b
Sasa Nedic <svetasmak0@gmail.com>
2024-11-10 20:19:22 +0100

    Add length in LegtnComments component

--- src/components/Details/addComment.tsx/LengthComments.tsx
+++ src/components/Details/addComment.tsx/LengthComments.tsx
@@ -1,9 +1,9 @@
 import Button from "../../UX/Button";
 
-const LengthComments = () => {
+const LengthComments = ({length}:any) => {
     return(
      <div className="w-full flex items-center justify-between">
-       <p className="text-gray-500 font-light">225 characters left</p>
+       <p className="text-gray-500 font-light">{length}/225 max characters </p>
        <Button type="addFeedBack">Post Comment</Button>
      </div>
     )
