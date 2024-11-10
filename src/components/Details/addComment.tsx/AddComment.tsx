da3bc5b3047f27e7e31702f0d4911571aeb976db
Sasa Nedic <svetasmak0@gmail.com>
2024-11-10 19:37:13 +0100

    Add textarea in AddComment component

--- src/components/Details/addComment.tsx/AddComment.tsx
+++ src/components/Details/addComment.tsx/AddComment.tsx
@@ -5,6 +5,10 @@ const AddComment = () => {
         <Wrapper type="feedBackComments">
          <p>SASA</p>
          <p>SASA</p>
+         <textarea 
+           className="w-full p-2 border rounded-lg bg-gray-200 text-gray-700 h-32 mb-6"
+           placeholder="Add comment"
+          />
         </Wrapper>
           
     )
