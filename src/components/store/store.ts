40fc8e5e839cb00a4c86653b56bb83ae0074a4a5
Sasa Nedic <svetasmak0@gmail.com>
2024-11-14 15:09:34 +0100

    Create new slice => userSlice

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoryReducer from './slices/categorySlice';
 import feedbackReducer from './slices/feedBackSlice'; 
+import userSliceReducer from './slices/feedBackSlice'; 
 
 const store = configureStore({
   reducer: {
     categories: categoryReducer,
     feedback: feedbackReducer, 
+    user: userSliceReducer,
   },
 });
 
