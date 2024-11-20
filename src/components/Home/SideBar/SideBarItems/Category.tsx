3bc8d5dac987fb33c12c806c3037c53ccc7df336
Sasa Nedic <svetasmak0@gmail.com>
2024-11-20 12:18:46 +0100

    Create some changes in components

--- src/components/Home/SideBar/SideBarItems/Category.tsx
+++ src/components/Home/SideBar/SideBarItems/Category.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Wrapper from "../../../UX/Wrapper";
 import { fetchCategory } from "../../../../supabase/supabaseFunctions";
 import SingleCategory from "../../../UX/SingleCategory";
+import { useSelector } from "react-redux";
 
 const Category = () => {
     const [categories, setCategories] = useState([]);
@@ -19,15 +20,31 @@ const Category = () => {
         getCategory(); 
     }, []);
 
+    // const handleCategorySelect = (selectedCategory) => {
+    //     console.log(selectedCategory, "Odabrana kategorija");
+    // };
+
+    // const feedbacks = useSelector((state) => state.categories.items).map((category) => category.Category)
+    // const renderCategoryHandler = () => {
+    //     feedbacks.map((feedback) => {
+    //         feedback.Category === 
+    //     })
+    // }
+    // const  getSelectedCategory = () => {
+        
+    // }
     return (
         <Wrapper type="sideBarItem">
             <ul className="text-sm h-full flex flex-wrap justify-start">
                 {categories.map((category, index) => (
-                    <SingleCategory key={index} category={category} />
+                    <SingleCategory 
+                        key={index} 
+                        category={category} 
+                    />
                 ))}
             </ul>
         </Wrapper>
     );
-}
+};
 
 export default Category;
--- src/components/UX/SingleCategory.tsx
+++ src/components/UX/SingleCategory.tsx
@@ -1,17 +1,27 @@
 import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
 
 const SingleCategory = ({ category }) => {
-    console.log(category, 'trebalo bi da je single category')
+     const feedbacks = useSelector((state) => state.categories.items).map((category) => category.Category)
+    const selectedCategoryHandler = () => {
+        feedbacks.map((feedback) => {
+            if(feedback.Category === category){
+                console.log('radi')
+            }
+        })
+    }
+
     return (
         <motion.li
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5, ease: "easeOut" }}
             className="bg-[#f2f4ff] text-xs rounded-lg px-3 py-1.5 font-semibold leading-[19px] list-none text-[#4661e6] mt-3 mr-3 cursor-pointer hover:bg-blue-200"
+            onClick={selectedCategoryHandler} 
         >
             {category}
         </motion.li>
     );
-}
+};
 
 export default SingleCategory;
