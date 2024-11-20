9e6d33322e2071261499cb55e4638278f50c2a31
Sasa Nedic <svetasmak0@gmail.com>
2024-11-20 22:06:56 +0100

    Finish EmptyFeedBack component

--- src/components/UX/EmptyFeedback.tsx
+++ src/components/UX/EmptyFeedback.tsx
@@ -1,4 +1,3 @@
-import { FiSearch } from "react-icons/fi";
 import Button from "./Button";
 import Icon from "./Icon";
 import { useNavigate } from "react-router-dom";
@@ -11,10 +10,10 @@ const EmptyFeedback = () => {
       }
     return(
         <Wrapper type="feedBackContainer">
-            <div className="flex flex-col items-center justify-center h-[430px] px-28">
-             <Icon name='search' size={48} color="blue"/>
-              <p>There is no feedback yet.</p>
-              <p>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
+            <div className="flex flex-col items-center justify-center h-[430px] px-24 gap-2">
+             <Icon name='search' size={84} color="#464f79"/>
+              <p className="font-bold text-2xl leading-9 text-center tracking-tight text-[#464f79] mt-6">There is no feedback yet.</p>
+              <p className="font-normal text-base leading-6 text-center text-[#647196] mb-6">Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
               <Button type="addFeedBack" onClick={feedBackPageHandler}>
                     <Icon name="plus" size={14} color="white" />
                     <span className="text-sm ml-1">Add Feedback</span>
