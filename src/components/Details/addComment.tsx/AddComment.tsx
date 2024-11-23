3df8b39d36c350243200201dad9c296ddc86a329
Sasa Nedic <svetasmak0@gmail.com>
2024-11-23 17:02:25 +0100

    Create controlled component

--- src/components/Details/addComment.tsx/AddComment.tsx
+++ src/components/Details/addComment.tsx/AddComment.tsx
@@ -5,60 +5,65 @@ import LengthComments from "./LengthComments";
 import Textarea from "../../UX/Textarea";
 import { useDispatch, useSelector } from "react-redux";
 import { postComment } from "../../../supabase/supabaseFunctions";
-import { addComment, setComments } from "../../store/slices/feedBackSlice";
+import { addComment } from "../../store/slices/feedBackSlice";
+import { toast } from "react-toastify";
 
-const AddComment = ({id}) => {
-    const [lengthComment, setLengthComment] = useState(225);
-    const [updateComment, setUpdateComment] = useState({});
-    const currentUser = useSelector((user) => user.user.user);
-    const dispatch = useDispatch()
+const AddComment = ({ id }) => {
+  const [lengthComment, setLengthComment] = useState(225);
+  const [updateComment, setUpdateComment] = useState({ user_comment: "" }); 
+  const currentUser = useSelector((user) => user.user.user);
+  const dispatch = useDispatch();
 
-    const updateCommentHandler = (e) => {
-        const comment = e.target.value;
-        console.log('comment za text-area', comment);
-        console.log('postComment objekat', updateComment)
-        setUpdateComment({
-            user: currentUser.full_name,
-            user_name: currentUser.user_name,
-            user_image: currentUser.user_image,
-            user_comment: comment,
-            feedback_id: id
-        });
-    };
+  const updateCommentHandler = (e) => {
+    const comment = e.target.value;
+    setUpdateComment({
+      ...updateComment, 
+      user: currentUser.full_name,
+      user_name: currentUser.user_name,
+      user_image: currentUser.user_image,
+      user_comment: comment, 
+      feedback_id: id,
+    });
+  };
 
-    const postCommentHandler = async() => {
-       const newComment = await postComment(updateComment)
-       if(newComment){
-        dispatch(addComment(newComment))
-         }
-       }
+  const postCommentHandler = async () => {
+    const newComment = await postComment(updateComment);
+    if (newComment) {
+      dispatch(addComment(newComment));
+      toast.success("You added a new comment!");
+      setUpdateComment({ user_comment: "" });
+      setLengthComment(225);
+    }
+  };
 
-    const updateLengthComment = (e:any) => {
-        const remainingLength = 225 - e.target.value.length;
-        setLengthComment(remainingLength >= 0 ? remainingLength : 0);
-    };
+  const updateLengthComment = (e) => {
+    const remainingLength = 225 - e.target.value.length;
+    setLengthComment(remainingLength >= 0 ? remainingLength : 0);
+  };
 
-    const handleChange = (e:any) => {
-        updateLengthComment(e);
-        updateCommentHandler(e);
-    };
+  const handleChange = (e) => {
+    updateLengthComment(e);
+    updateCommentHandler(e);
+  };
 
-    return (
-        <Wrapper type="feedBackComments">
-            <FeedBackTitle
-                title="Add Comment"
-                subtitle="Share with us your opinion about this feedback"
-            />
-            <Textarea
-                placeholder="Add comment"
-                rows={4}
-                additionalStyles="bg-gray-200 text-gray-700 h-28 mb-6"
-                onChange={handleChange} 
-                maxLength={225}
-            />
-            <LengthComments length={lengthComment} postCommentHandler={postCommentHandler} />
-        </Wrapper>
-    );
+  return (
+    <Wrapper type="feedBackComments">
+      <FeedBackTitle
+        title="Add Comment"
+        subtitle="Share with us your opinion about this feedback"
+      />
+      <Textarea
+        placeholder="Add comment"
+        rows={4}
+        value={updateComment.user_comment} 
+        additionalStyles="bg-gray-200 text-gray-700 h-28 mb-6"
+        onChange={handleChange}
+        maxLength={225}
+      />
+      <LengthComments length={lengthComment} postCommentHandler={postCommentHandler} />
+    </Wrapper>
+  );
 };
 
 export default AddComment;
+
--- src/components/UX/Textarea.tsx
+++ src/components/UX/Textarea.tsx
@@ -1,6 +1,6 @@
 import { TextareaProps } from "../types/types";
 
-const Textarea = ({ placeholder, rows = 3, additionalStyles = "", onChange, maxLength }:TextareaProps) => {
+const Textarea = ({ placeholder, rows = 3, additionalStyles = "", onChange, maxLength, value }:TextareaProps) => {
     const baseStyles = "w-full p-3 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500";
 
     return (
@@ -10,6 +10,7 @@ const Textarea = ({ placeholder, rows = 3, additionalStyles = "", onChange, maxL
             placeholder={placeholder}
             onChange={onChange}
             maxLength={maxLength}
+            value={value}
         />
     );
 };
