191c3d4c14c2b28d574851766463cd5d8a97dcd9
Sasa Nedic <svetasmak0@gmail.com>
2024-11-24 21:45:31 +0100

    Create state and controlled Teaxtarea component

--- src/components/Details/feedBackComments/PostReplay.tsx
+++ src/components/Details/feedBackComments/PostReplay.tsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import Button from "../../UX/Button";
 import Textarea from "../../UX/Textarea";
 
 const PostReplay = () => {
+  const [commentText, setCommentText] = useState("");
+
   const commentAnswerHandler = (e) => {
-    const commentText = e.target.value;
+    setCommentText(e.target.value);
     console.log(commentText, "commentText");
   };
+
+  //   const postReplyHandler = () => {
+  //     console.log("Posting reply:", commentText);
+  //   };
+
   return (
     <div className="w-full mt-6 rounded-lg">
       <Textarea
         placeholder="Your text here..."
         additionalStyles="bg-gray-50"
         onChange={commentAnswerHandler}
+        value={commentText}
       />
       <div className="flex justify-end">
-        <Button type="addFeedBack">Post Reply</Button>
+        <Button type="addFeedBack" onClick={postReplyHandler}>
+          Post Reply
+        </Button>
       </div>
     </div>
   );
