751f37272b429d2502ccb2ce2d46d1bcb94a3fa1
Sasa Nedic <svetasmak0@gmail.com>
2024-11-24 23:49:53 +0100

    Create changes

--- src/components/Roadmap/RoadmapPage.tsx
+++ src/components/Roadmap/RoadmapPage.tsx
@@ -3,37 +3,38 @@ import Action from "../UX/Action";
 import Button from "../UX/Button";
 
 const RoadmapPage = () => {
-  const navigation = useNavigate()
+  const navigation = useNavigate();
 
   const feedBackPageHandler = () => {
-    navigation('/add')
-  }
- 
+    navigation("/add");
+  };
+
   const homePageHandler = () => {
-    navigation('/')
-}
-    return(
-        // <Wrapper type="pageWrapper">
-        <div className="bg-gray-100 flex gap-5 flex-col h-screen px-32 pt-16 font-serif">
-            <div className="flex items-center justify-between bg-[#464f79] p-6 rounded-lg mb-4">
-                <div className="">
-                    <Action 
-                        iconName="back" 
-                        iconColor="white" 
-                        iconSize={14} 
-                        text="Go back"
-                        labelType='backRoadMap'
-                        onClick={() => homePageHandler()}
-                    />
-                    <h2 className="text-white mt-2 font-semibold text-xl">Roadmap</h2>
-                </div>
-                <Button type="addFeedBack" onClick={() => feedBackPageHandler()}>
-                    Add Feedback
-                </Button>
-            </div>
+    navigation("/");
+  };
+  return (
+    // <Wrapper type="pageWrapper">
+    <div className="bg-gray-100 flex gap-5 flex-col h-screen px-32 pt-16 font-serif">
+      <div className="flex items-center justify-between bg-[#464f79] p-6 rounded-lg mb-4">
+        <div className="">
+          <Action
+            iconName="back"
+            iconColor="white"
+            iconSize={14}
+            text="Go back"
+            labelType="backRoadMap"
+            onClick={() => homePageHandler()}
+          />
+          <h2 className="text-white mt-2 font-semibold text-xl">Roadmap</h2>
         </div>
-        // </Wrapper>
-    )
-}
+        <Button type="addFeedBack" onClick={() => feedBackPageHandler()}>
+          Add Feedback
+        </Button>
+      </div>
+      <p clas>SASASA</p>
+    </div>
+    // </Wrapper>
+  );
+};
 
 export default RoadmapPage;
