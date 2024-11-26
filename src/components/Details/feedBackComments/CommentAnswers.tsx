c7f566794db09c0c9d34704bbabda33ca83b3d5e
Sasa Nedic <svetasmak0@gmail.com>
2024-11-26 18:47:02 +0100

    Create changes

--- src/components/Details/feedBackComments/CommentAnswers.tsx
+++ src/components/Details/feedBackComments/CommentAnswers.tsx
@@ -9,14 +9,7 @@ const CommentAnswers = ({ answer }: CommentAnswersProps) => {
   console.log(answer, "jeeeedan");
   return (
     <div className="pl-8 border-l-2 border-gray-300">
-      <CommentCard
-        user_image={answer.comment_user_answer?.user_image || ""}
-        user={answer.comment_user_answer?.user || "Anonymous"}
-        user_name={answer.comment_user_answer?.user_name || ""}
-        user_comment={answer.answer}
-        answers={[]}
-        answerId={answer.comment_user_answer?.id || ""}
-      />
+      <Commen
     </div>
   );
 };
