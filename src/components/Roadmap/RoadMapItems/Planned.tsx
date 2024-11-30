e0579439577ecb929478c582fdcad9f26d446729
Sasa Nedic <svetasmak0@gmail.com>
2024-11-30 23:09:55 +0100

    Create changes in Planned compoenents

--- src/components/Roadmap/RoadMapItems/Planned.tsx
+++ src/components/Roadmap/RoadMapItems/Planned.tsx
@@ -3,13 +3,17 @@ import UserFeedBack from "../../Home/Content/UserFeedBack";
 const Planned = ({ feedbacks }) => {
   return (
     <div className="w-full">
-      <span>({feedbacks.length})</span>
+      <span>
+        ({feedbacks.filter((item) => item.type === "Planned").length})
+      </span>
       <h3 className="text-lg font-semibold">Planned</h3>
       <p className="text-sm text-gray-500">Ideas prioritized for research</p>
       <div className="mt-4">
-        {feedbacks.map((item) => (
-          <UserFeedBack key={item.id} item={item} />
-        ))}
+        {feedbacks
+          .filter((item) => item.type === "Planned")
+          .map((item) => (
+            <UserFeedBack key={item.id} item={item} />
+          ))}
       </div>
     </div>
   );
