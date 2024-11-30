54a359dbd371d0c541d5d288d3855b28c8248e99
Sasa Nedic <svetasmak0@gmail.com>
2024-11-30 23:03:47 +0100

    Changes in RoadMapContent

--- src/components/Roadmap/RoadMapItems/RoadMapContent.tsx
+++ src/components/Roadmap/RoadMapItems/RoadMapContent.tsx
@@ -1,16 +1,37 @@
-import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { fetchFeedbacksWithAllRelations } from "../../../supabase/supabaseFunctions";
+import Planned from "./Planned";
 import InProgress from "./InProgress";
 import Live from "./Live";
-import Planned from "./Planned";
 
 const RoadMapContent = () => {
-  const feedbacks = useSelector((state) => state.feedback.feedback);
-  console.log(feedbacks, "iz roadmapa feedbacks");
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  useEffect(() => {
+    const fetchFeedbacksHandler = async () => {
+      const feedbacksData = await fetchFeedbacksWithAllRelations();
+      setFeedbacks(feedbacksData);
+    };
+
+    fetchFeedbacksHandler();
+  }, []);
+
+  // Filtriraj podatke po tipu
+  const plannedFeedbacks = feedbacks.filter(
+    (feedback) => feedback.status === "Planned"
+  );
+  const inProgressFeedbacks = feedbacks.filter(
+    (feedback) => feedback.status === "In Progress"
+  );
+  const liveFeedbacks = feedbacks.filter(
+    (feedback) => feedback.status === "Live"
+  );
+
   return (
     <div className="flex justify-between gap-8">
-      <Planned />
-      <InProgress />
-      <Live />
+      <Planned feedbacks={plannedFeedbacks} />
+      <InProgress feedbacks={inProgressFeedbacks} />
+      <Live feedbacks={liveFeedbacks} />
     </div>
   );
 };
