4359e416040292b69ad0457278160912d0734e2f
Sasa Nedic <svetasmak0@gmail.com>
2024-12-02 17:00:22 +0100

    Create changes

--- src/components/Home/Content/ContentBodyItems.tsx/FeedbackType.tsx
+++ src/components/Home/Content/ContentBodyItems.tsx/FeedbackType.tsx
@@ -7,7 +7,9 @@ const FeedbackType = ({ type }) => {
 
   return (
     <p className="flex items-center gap-2 text-sm text-gray-500 mb-2">
-      <span className={`w-2 h-2 rounded-full ${bgColorHanlder[type]} `}></span>
+      <span
+        className={`w-[25px] h-2 rounded-full ${bgColorHanlder[type]} `}
+      ></span>
       {type}
     </p>
   );
