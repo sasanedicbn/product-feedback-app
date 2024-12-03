f8025b8f2c92a3ce026977cbf37e13a55be51b8c
Sasa Nedic <svetasmak0@gmail.com>
2024-12-03 16:31:59 +0100

    Create changes in Roadmap component

--- src/components/Home/SideBar/SideBar.tsx
+++ src/components/Home/SideBar/SideBar.tsx
@@ -1,14 +1,17 @@
+import { useSelector } from "react-redux";
 import Category from "./SideBarItems/Category";
 import Roadmap from "./SideBarItems/Roadmap";
 import TitleSideBar from "./SideBarItems/TitleSideBar";
 
 const SideBar = () => {
-    return(
-        <div className="w-[250px]">
-         <TitleSideBar/>
-         <Category/>
-         <Roadmap/>
-        </div>
-    )
-}
+  const feedbacks = useSelector((state) => state.feedback.feedback);
+
+  return (
+    <div className="w-[250px]">
+      <TitleSideBar />
+      <Category />
+      <Roadmap feedbacks={feedbacks} />
+    </div>
+  );
+};
 export default SideBar;
--- src/components/Home/SideBar/SideBarItems/Roadmap.tsx
+++ src/components/Home/SideBar/SideBarItems/Roadmap.tsx
@@ -1,36 +1,58 @@
-import { Link } from 'react-router-dom'; 
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import Wrapper from "../../../UX/Wrapper";
 import RoadmapItem from "./RoadmapItem/RoadmapItem";
+import { useSelector } from "react-redux";
 
 const Roadmap = () => {
-    return (
-        <Wrapper type="sideBarItem">
-            <motion.section
-                initial={{ opacity: 0, x: -70 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.62, ease: "easeOut" }}
-                className="flex justify-between items-center color-bg mb-5"
-            >
-                <p className="font-semibold text-blue-900">RoadMap</p>
-                <Link to="/roadmap" className="text-xs text-blue-500 border-b border-blue-500">View</Link> 
-            </motion.section>
+  const feedbacks = useSelector((state) => state.categories.items);
+  const inProgressFeedbacks = feedbacks.filter(
+    (item) => item.type === "Progress"
+  );
+  const liveFeedbacks = feedbacks.filter((item) => item.type === "Live");
+  const plannedFeedbacks = feedbacks.filter((item) => item.type === "Planned");
 
-            <motion.section
-                initial={{ opacity: 0, x: -70 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{
-                    duration: 0.62,
-                    ease: "easeOut",
-                    staggerChildren: 0.2
-                }}
-            >
-                <RoadmapItem label="Planned" count={2} status="planned" />
-                <RoadmapItem label="In-Progress" count={3} status="inProgress" />
-                <RoadmapItem label="Live" count={1} status="live" />
-            </motion.section>
-        </Wrapper>
-    );
-}
+  console.log(inProgressFeedbacks.length, "iz roadaa");
+  return (
+    <Wrapper type="sideBarItem">
+      <motion.section
+        initial={{ opacity: 0, x: -70 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.62, ease: "easeOut" }}
+        className="flex justify-between items-center color-bg mb-5"
+      >
+        <p className="font-semibold text-blue-900">RoadMap</p>
+        <Link
+          to="/roadmap"
+          className="text-xs text-blue-500 border-b border-blue-500"
+        >
+          View
+        </Link>
+      </motion.section>
+
+      <motion.section
+        initial={{ opacity: 0, x: -70 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{
+          duration: 0.62,
+          ease: "easeOut",
+          staggerChildren: 0.2,
+        }}
+      >
+        <RoadmapItem
+          label="Planned"
+          count={plannedFeedbacks.length}
+          status="planned"
+        />
+        <RoadmapItem
+          label="In-Progress"
+          count={inProgressFeedbacks.length}
+          status="inProgress"
+        />
+        <RoadmapItem label="Live" count={liveFeedbacks.length} status="live" />
+      </motion.section>
+    </Wrapper>
+  );
+};
 
 export default Roadmap;
