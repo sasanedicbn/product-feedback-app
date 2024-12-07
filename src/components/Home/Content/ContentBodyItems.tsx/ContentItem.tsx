7199b378e005a9e45e4d82e4e59778287ab2b610
Sasa Nedic <svetasmak0@gmail.com>
2024-12-07 23:24:41 +0100

    Create caganges

--- src/components/Home/Content/ContentBodyItems.tsx/ContentItem.tsx
+++ src/components/Home/Content/ContentBodyItems.tsx/ContentItem.tsx
@@ -16,7 +16,9 @@ const ContentItem = ({
 
   return (
     <div onClick={handleItemClick} className="text-dark-blue  cursor-pointer">
-      <h2 className="text-blue-950 opacity-90  text-xl font-bold">{title}</h2>
+      <h2 className="md:text-blue-950 opacity-90  text-xl font-bold sm:text-orange-300">
+        {title}
+      </h2>
       <p className="text-gray-500 text-sm font-light my-1">{feedback}</p>
       <div className="flex flex-wrap justify-start">
         <SingleCategory category={category} />
