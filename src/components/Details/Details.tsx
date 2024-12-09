018c1b67c6a8c40cfaf0f67ae706460c8b48bdf4
Sasa Nedic <svetasmak0@gmail.com>
2024-12-09 23:21:46 +0100

    Create changes in Details component

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -32,7 +32,14 @@ const Details = () => {
     <div className="bg-gray-100 flex gap-5 flex-col pb-12 justify-center pt-4 font-serif md:pt-16 sm:pt-8">
       <div className="px-2 md:max-w-[725px] md: mx-auto sm: max-w-[650px]">
         <div className="flex items-center justify-between mb-4">
-          <Ac labelType="back" onClick={() => homePageHanlder()} />
+          <Action
+            iconName="back"
+            iconColor="gray"
+            iconSize={14}
+            text="Go back"
+            labelType="back"
+            onClick={() => homePageHanlder()}
+          />
           <Button type="editFeedBack" onClick={() => console.log("Cancelled")}>
             Edit Feedback
           </Button>
