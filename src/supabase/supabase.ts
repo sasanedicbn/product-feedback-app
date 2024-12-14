9306d5a3dc11d3c70abec1b527d0b3b52d37c7ae
Sasa Nedic <svetasmak0@gmail.com>
2024-12-14 13:09:56 +0100

    Create env file

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://uybqjtampdbwtgkkapwg.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5YnFqdGFtcGRid3Rna2thcHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0OTYzMjcsImV4cCI6MjA0NjA3MjMyN30.23hezEfRpS2jQW4iZ5lwp27W4HuJrFks4ak4fbDjlPc";
-
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
+console.log(supabaseUrl, 'supabaseUrl', supabaseKey, 'supabaseKey')
 export const supabase = createClient(supabaseUrl, supabaseKey);
