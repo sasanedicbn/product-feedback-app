8eed242bc566b14153bf3b63544333e2da9dd050
Sasa Nedic <svetasmak0@gmail.com>
2024-12-14 18:19:05 +0100

    Create userAction slice

---
+++ src/components/store/slices/userAction.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+};
+
+const userAction = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+   
+  },
+});
+
+export const {  } = userAction.actions;
+export default userAction.reducer;
