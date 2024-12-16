bb82ff061c4f40ed2399646003520e460f068aa7
Sasa Nedic <svetasmak0@gmail.com>
2024-12-16 18:08:58 +0100

    Create changes in categorySlice

--- src/components/store/slices/categorySlice.ts
+++ src/components/store/slices/categorySlice.ts
@@ -2,33 +2,38 @@ import { createSlice } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import { Feedback } from '../../types/types';
 
+const initialState: {
+  items: Feedback[];
+  currentCategory: string;
+  currentSort: string;
+} = {
+  items: [],
+  currentCategory: '',
+  currentSort: '',
+};
+
 const categorySlice = createSlice({
   name: 'categories',
-  initialState: {
-    items: [], 
-    currentCategory: '',
-    currentSort: ''
-  },
+  initialState,
   reducers: {
     setCategories: (state, action) => {
-      state.items = action.payload; 
+      state.items = action.payload;
     },
     addCategory: (state, action) => {
-      state.items = action.payload
+      state.items = action.payload;
     },
     setCurrentCategory: (state, action) => {
-      state.currentCategory = action.payload
+      state.currentCategory = action.payload;
     },
     setCurrentSort: (state, action) => {
-      state.currentSort = action.payload
+      state.currentSort = action.payload;
     },
     addLike: (state, action) => {
       const feedbackId = action.payload;
       const feedback = state.items.find((item) => item.id === feedbackId);
-    
       if (feedback) {
         if (feedback.isLiked) {
-          feedback.upvotes -= 1; 
+          feedback.upvotes -= 1;
           toast.success('Successfully disliked feedback');
         } else {
           feedback.upvotes += 1;
