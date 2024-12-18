f1a574fc0405b851d94316b0f5d98b5af64f384b
Sasa Nedic <svetasmak0@gmail.com>
2024-12-18 17:02:50 +0100

    .

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = "https://uybqjtampdbwtgkkapwg.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5YnFqdGFtcGRid3Rna2thcHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0OTYzMjcsImV4cCI6MjA0NjA3MjMyN30.23hezEfRpS2jQW4iZ5lwp27W4HuJrFks4ak4fbDjlPc";
 
 console.log(supabaseUrl, supabaseUrl)
 console.log(supabaseKey, supabaseKey)
